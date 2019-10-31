be0e9b615b8c31a24be1f3c868537c9004b09f4f
motaku <motakumotamota@gmail.com>
2019-10-31 12:03:08 +0900

    Finish adding task function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,26 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  #tasks/newにアクセスする時に使われる
+  def new
+    @task = Task.new
+  end
+
+  #taskを追加する時に使われる
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+  #外部からメソッドの使用をできないようにする
+
+  #ストロングパラメータ
+  # viewから得られる必要な値だけを許可して使う
+  # require(:task)taskの中の情報のみを限定
+  # permit(:title)taskの中のtitileのみ情報を限定
+  def task_params
+    params.require(:task).permit(:title)
+  end
 end
